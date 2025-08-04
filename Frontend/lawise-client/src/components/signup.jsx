dcbc41933237a8d9d1b1d0270f127d0e02889888
Vansh Sachdeva <vanshsachdeva900@gmail.com>
2025-08-04 13:46:54 +0530

    .

--- Frontend/lawise-client/src/components/signup.jsx
+++ Frontend/lawise-client/src/components/signup.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useContext } from 'react';
-import axios from 'axios';
-import config from '../config';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '../context/authcontext';
 
